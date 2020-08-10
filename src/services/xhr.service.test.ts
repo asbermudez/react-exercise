e1ef09acc05660dc48ea337ecaddf870c5fe6ee9
Aram Sabatés <asbermudez@gmail.com>
2020-08-10 20:14:51 +0200

    XHR service adjust types

--- src/services/xhr.service.test.ts
+++ src/services/xhr.service.test.ts
@@ -1,4 +1,3 @@
-import React from 'react';
 import xhrService from './xhr.service';
 
 describe('XHRService', () => {
--- src/services/xhr.service.ts
+++ src/services/xhr.service.ts
@@ -1,7 +1,7 @@
 import { forEach as _forEach, reduce as _reduce } from 'lodash';
 
 class XHRService {
-  get<T>(url: string, params?: Record<string, any>): Promise<T> {
+  get<T>(url: string, params?: Record<string, unknown>): Promise<T> {
     return fetch(`${url}${this.getUrlParams(params)}`, { method: 'GET' })
       .then(this.handleResponseStatus)
       .then((response) => this.parseResponse<T>(response));
@@ -17,12 +17,13 @@ class XHRService {
   private parseResponse<T>(response: Response): Promise<T> {
     if (response.status === 204) {
       // 204 is an empty response so we return an empty promise
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       return Promise.resolve() as Promise<any>;
     }
     return response.json();
   }
 
-  private getUrlParams(params: Record<string, any> = {}): string {
+  private getUrlParams(params: Record<string, unknown> = {}): string {
     let urlParams = '';
     let separator = '?';
     _forEach(params, (param, key) => {
@@ -34,7 +35,7 @@ class XHRService {
     return urlParams;
   }
 
-  private URIEncode(key: string, param: any): string {
+  private URIEncode(key: string, param: unknown): string {
     if (Array.isArray(param)) {
       return _reduce(param, (query, item) => `${query}&${this.URIEncode(`${key}`, item)}`, '').replace(/^&/iu, '');
     }
@@ -44,7 +45,7 @@ class XHRService {
         '',
       );
     }
-    return `${key}=${encodeURIComponent(param)}`;
+    return `${key}=${encodeURIComponent(param as string | number | boolean)}`;
   }
 }
 
