9139353f9395ff078bd8815931c930f9c1cebdc1
Aram Sabatés <asbermudez@gmail.com>
2020-08-11 04:05:42 +0200

    Home test

---
+++ src/pages/home/Home.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Home from './Home';
+import filmsService from '../../services/films.service';
+import { executeAsync } from '../../test-utils';
+
+jest.spyOn(React, 'useEffect').mockImplementation((f) => f());
+
+describe('Home page', () => {
+  it('should have the right title and the film list component', async () => {
+    // GIVEN
+    const wrapper = mount(<Home />);
+
+    // THEN
+    expect(wrapper.find(`.home__title`).text()).toBe('Ghibli film DB');
+    expect(wrapper.find(`.film-list`).length).toBe(1);
+  });
+});
